85c45df432fe149b113242cb88659565be4f8811
Thi Ngoc Anh TRAN <nganh.taka@gmail.com>
2022-09-25 22:36:04 +0200

    feat: add another enter_the_matrix_2 1/7

---
+++ prologin/2022_qualification/enter_the_matriks_2.hs
@@ -0,0 +1,52 @@
+-- https://prologin.org/train/2022/qualification/enter-the-matriks
+canGetSum :: Int -> [Int] -> Int -> Int -> Int -> [Int]
+canGetSum 0 _ _ _ _ = []
+canGetSum _ [] _ _ _ = []
+canGetSum goal [x] _ _ _ = if goal == x then [x] else []
+canGetSum goal liste _ 0 nbTotal = canGetSum goal (tail liste) 0 ((length liste)-1) nbTotal
+canGetSum goal liste i step nbTotal = 
+  if (goal == (sum (take step (drop i liste)))) 
+  then (take step (drop i liste)) 
+  else (if ((i+step) <= nbTotal) 
+        then (canGetSum goal liste (i+1) step nbTotal) 
+        else (canGetSum goal liste 0 (step-1) nbTotal))
+
+calculMagic :: Int -> [Int]-> Int -> Int -> Int -> [[Int]]
+calculMagic 0 _ _ _ _  = [[]]
+calculMagic _ [] _ _ _ = [[]]
+calculMagic goal liste i step nbTotal = 
+  if (mod goal (sum (take step (drop i liste)))) == 0 
+  then [(take step (drop i liste)), (canGetSum (div goal (sum (take step (drop i liste)))) liste 0 nbTotal nbTotal)] 
+  else (if ((i+step) <= nbTotal) 
+        then (calculMagic goal liste (i+1) step nbTotal) 
+        else (calculMagic goal liste 0 (step-1) nbTotal))
+
+listIntToString :: [Int] -> String
+listIntToString [x] = show x
+listIntToString (x:xs) = (listIntToString [x]) ++ " " ++ (listIntToString xs)
+
+magicToString :: [[Int]] -> String
+magicToString [[]] = "IMPOSSIBLE"
+magicToString [[],_] = "IMPOSSIBLE"
+magicToString [_,[]] = "IMPOSSIBLE"
+magicToString [x, y] = if (length x > length y) || (sum x) >= (sum y) then (listIntToString x) ++ "\n" ++ (listIntToString y)
+  else (listIntToString y) ++ "\n" ++ (listIntToString x)
+
+resoudre :: Int     -- ^ le nombre magique
+         -> Int     -- ^ la longueur du code la Matriks
+         -> [Int]   -- ^ le code de la Matriks
+         -> String  -- ^ TODO
+-- Les deux clés (chacune sur une ligne) ou le message "IMPOSSIBLE".
+resoudre 0 _ _ = "IMPOSSIBLE"
+resoudre _ 0 _ = "IMPOSSIBLE"
+resoudre _ _ [] = "IMPOSSIBLE"
+resoudre x 1 [y] = if (div x y == y) then magicToString ([[y], [y]]) else "IMPOSSIBLE"
+resoudre x n l = if (n /= (length l)) then "IMPOSSIBLE"
+  magicToString (calculMagic x l 0 n n)
+
+main :: IO ()
+main = do
+  x <- fmap read getLine
+  n <- fmap read getLine
+  l <- fmap (map read . words) getLine
+  putStrLn $ resoudre x n l
