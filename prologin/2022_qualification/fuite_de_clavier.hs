7a2992eac46f83f07eeb778d521f21a9fbfd86af
Thi Ngoc Anh TRAN <nganh.taka@gmail.com>
2022-09-26 02:10:28 +0200

    feat: optiomize checkRules 4/6

--- prologin/2022_qualification/fuite_de_clavier.hs
+++ prologin/2022_qualification/fuite_de_clavier.hs
@@ -8,14 +8,21 @@ hasElement xs f
     | True `elem` (map f xs) = True
     | otherwise             = False
 
-checkRules :: [Char] -> Bool
-checkRules x = (hasElement x isUpper) && (hasElement x isLower) 
-                && (hasElement x isDigit) && ((hasElement x isPunctuation) || (hasElement x isCharacterSpecial))
-
+checkRules :: [Char] -> [Bool] -> Bool
+checkRules _ [True, True, True, True] = True
+checkRules [] bool = all (== True) bool
+checkRules (x:xs) [a,b,c,d] = if all (== True) [a,b,c,d] then True else
+    checkRules xs [
+        if a == True then True else (isUpper x),
+        if b == True then True else (isLower x),
+        if c == True then True else (isDigit x),
+        if d == True then True else ((isPunctuation x) || (isCharacterSpecial x))
+    ]
+    
 getAllChaine :: [Char] -> Int -> Int -> Int -> Int
 getAllChaine _ 0 _ result = result
 getAllChaine xs i pwSize result = let var = (take pwSize (drop i xs)) in do
-    if (checkRules var) then getAllChaine xs (i-1) pwSize (result+1)
+    if (checkRules var [False, False, False, False]) then getAllChaine xs (i-1) pwSize (result+1)
     else getAllChaine xs (i-1) pwSize result
 
 fuiteDeClavier :: Int     -- ^ taille de la chaîne
@@ -24,7 +31,10 @@ fuiteDeClavier :: Int     -- ^ taille de la chaîne
                -> String  -- ^ TODO
 -- afficher le nombre de mots de passes possibles parmi la chaîne
 fuiteDeClavier n k chaine
-    | n < 4 = "0"
+    | k < 4 = "0"
+    | n < k = "0"
+    | n > 1000000 = "0"
+    | k > 10000 = "0"
     | otherwise = show (getAllChaine chaine (n-k) k 0)
 
 main :: IO ()
