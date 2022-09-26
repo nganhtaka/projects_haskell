aa80fea9555e0dbac2767c29d8347c2b4d43ef41
Thi Ngoc Anh TRAN <nganh.taka@gmail.com>
2022-09-26 02:54:45 +0200

    feat: finish fuite de clavier

--- prologin/2022_qualification/fuite_de_clavier.hs
+++ prologin/2022_qualification/fuite_de_clavier.hs
@@ -1,7 +1,4 @@
-import Data.Char (isUpper, isLower, isDigit, isPunctuation)
-
-isCharacterSpecial :: Char -> Bool
-isCharacterSpecial c = if (c `elem` "$+<=>^`|~") then True else False
+import Data.Char (isUpper, isLower, isDigit, isPunctuation, isSymbol)
 
 hasElement :: [Char] -> (Char -> Bool) -> Bool
 hasElement xs f
@@ -12,15 +9,14 @@ checkRules :: [Char] -> [Bool] -> Bool
 checkRules _ [True, True, True, True] = True
 checkRules [] bool = all (== True) bool
 checkRules (x:xs) [a,b,c,d] = if all (== True) [a,b,c,d] then True else
-    checkRules xs [
-        if a == True then True else (isUpper x),
-        if b == True then True else (isLower x),
-        if c == True then True else (isDigit x),
-        if d == True then True else ((isPunctuation x) || (isCharacterSpecial x))
-    ]
+    if a == False && (isUpper x) then checkRules xs [True,b,c,d] else
+        if b == False && (isLower x) then checkRules xs [a,True,c,d] else
+            if c == False && (isDigit x) then checkRules xs [a,b,True,d] else
+                if d == False && ((isPunctuation x) || (isSymbol x)) then checkRules xs [a,b,c,True] else
+                    checkRules xs [a,b,c,d]
     
 getAllChaine :: [Char] -> Int -> Int -> Int -> Int
-getAllChaine _ 0 _ result = result
+getAllChaine _ (-1) _ result = result
 getAllChaine xs i pwSize result = let var = (take pwSize (drop i xs)) in do
     if (checkRules var [False, False, False, False]) then getAllChaine xs (i-1) pwSize (result+1)
     else getAllChaine xs (i-1) pwSize result
